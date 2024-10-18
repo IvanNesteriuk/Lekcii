db59db89647fd11cf8fce0eb9ddf36a6c0aaff39
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2024-10-18 11:53:50 +0300

    -- P14

--- src/practice/p14/char_indexes.rs
+++ src/practice/p14/char_indexes.rs
@@ -1,6 +1,7 @@
+use itertools::Itertools;
 use std::collections::HashMap;
 
-fn char_indexes(text: String) -> HashMap<char, Vec<usize>> {
+fn char_indexes(text: &String) -> HashMap<char, Vec<usize>> {
     let mut outcome: HashMap<char, Vec<usize>> = HashMap::new();
     text.chars()
         .into_iter()
@@ -16,8 +17,25 @@ fn char_indexes(text: String) -> HashMap<char, Vec<usize>> {
     outcome
 }
 
+// with help of https://docs.rs/itertools/latest/itertools/
+fn char_indexes2(text: &String) -> HashMap<char, Vec<usize>> {
+    text.chars()
+        .into_iter()
+        .zip(1..)
+        .filter(|&(c, _)| c.is_alphabetic())
+        .map(|(c, idx)| (c.to_ascii_lowercase(), idx))
+        .into_group_map()
+}
+
 #[test]
 fn char_indexes_test() {
-    let indexes = char_indexes(String::from("Hello, world!"));
-    println!("{:?}", indexes);
+    let s = String::from("Hello, world!");
+
+    let indexes1 = char_indexes(&s);
+    println!("{:?}", indexes1);
+
+    let indexes2 = char_indexes2(&s);
+    println!("{:?}", indexes2);
+
+    assert_eq!(indexes1, indexes2);
 }
